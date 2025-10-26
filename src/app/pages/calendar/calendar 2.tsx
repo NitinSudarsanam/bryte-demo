9e2bf446d4d9c9ef876837a8b687edad3d85e8d9
acrylicpaintt <116538518+acrylicpaintt@users.noreply.github.com>
2025-10-26 17:26:27 -0400

    header and calendar

---
+++ src/app/pages/calendar/calendar 2.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import CalendarEmbed from "./embedcalendar";
+
+export default function Calendar() {
+  return (
+    <div>
+      <CalendarEmbed/>
+    </div>
+  );
+}
