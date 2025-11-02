d17f0ae7848cdc1369a015b265ced5055f9f5ee3
Nitin Sudarsanam <nitinsudarsanam@yahoo.com>
2025-11-02 16:38:02 -0500

    made components

---
+++ src/app/components/header/header.tsx
@@ -0,0 +1,84 @@
+"use client";
+import * as React from "react";
+import * as NavigationMenu from "@radix-ui/react-navigation-menu";
+import { Flex, Theme } from '@radix-ui/themes';
+import "./header.css";
+
+const Header: React.FC = () => {
+  return (
+    <Flex direction="column" gap="2">
+      <Theme>
+        <NavigationMenu.Root className="NavigationMenuRoot" orientation="vertical">
+          <NavigationMenu.List className="NavigationMenuList">
+            <NavigationMenu.Item>
+              <NavigationMenu.Link
+                className="NavigationMenuLink"
+                href="/"
+              >
+                Sun Logo
+              </NavigationMenu.Link>
+            </NavigationMenu.Item>
+
+            <NavigationMenu.Item>
+              <NavigationMenu.Link
+                className="NavigationMenuLink"
+                href="/pages/about"
+              >
+                About Us
+              </NavigationMenu.Link>
+            </NavigationMenu.Item>
+
+            <NavigationMenu.Item>
+              <NavigationMenu.Link
+                className="NavigationMenuLink"
+                href="/pages/faqs"
+              >
+                FAQ
+              </NavigationMenu.Link>
+            </NavigationMenu.Item>
+
+            <NavigationMenu.Item>
+              <NavigationMenu.Link
+                className="NavigationMenuLink"
+                href="/pages/parent-resources"
+              >
+                Resources
+              </NavigationMenu.Link>
+            </NavigationMenu.Item>
+
+            <NavigationMenu.Item>
+              <NavigationMenu.Link
+                className="NavigationMenuLink"
+                href="/pages/calendar"
+              >
+                Calendar
+              </NavigationMenu.Link>
+            </NavigationMenu.Item>
+
+            <NavigationMenu.Item>
+              <NavigationMenu.Link
+                className="NavigationMenuLink"
+                href="/pages/partners"
+              >
+                Our Partners
+              </NavigationMenu.Link>
+            </NavigationMenu.Item>
+
+            <NavigationMenu.Item>
+              <NavigationMenu.Link
+                className="JoinUsButton"
+                href="/pages/join"
+              >
+                Join Us!
+              </NavigationMenu.Link>
+            </NavigationMenu.Item>
+          </NavigationMenu.List>
+
+          <NavigationMenu.Viewport />
+        </NavigationMenu.Root>
+      </Theme>
+    </Flex>
+  );
+};
+
+export default Header;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import classNames from "classnames";
 import { Flex, Button, Theme } from '@radix-ui/themes';
 import { CaretDownIcon } from "@radix-ui/react-icons";
 import "./globals.css";
-import Header from './header';
+import Header from '@/app/components/header';
 export default function HomePage() {
   return (
       <div>
