c990bf78c688057996f12c20340730aaddf9f4de
acrylicpaintt <116538518+acrylicpaintt@users.noreply.github.com>
2025-12-07 22:24:18 -0500

    textbox to calendar

--- src/app/pages/calendar/page.tsx
+++ src/app/pages/calendar/page.tsx
@@ -2,6 +2,8 @@ import CalendarEmbed from "./calendar";
 import Header from "../../header";
 import "./calendar.css"
 
+import Section from "@/app/components/section/section";
+
 
 
 export default function Calendar() {
@@ -13,8 +15,24 @@ export default function Calendar() {
       <div className="calendar-container-container">
         <CalendarEmbed/>   
       </div>
-      
+
+      <Section
+        title="ETC CALENDAR"
+        content="BRYTE has received numerous awards since its inception in 2006. 
+Most recently, BRYTE won the 2017: Student Leadership Award: Best Collaboration from Brown University for our collaborative effort with Impulse Dance Company. We have also been the recipients of the 2013 Student Leadership Award: Forming Partnerships from Brown 
+
+BRYTE has received numerous awards since its inception in 2006. Most recently, BRYTE won the 2017: Student Leadership Award: Best Collaboration from Brown University for our collaborative effort with Impulse Dance Company. We have also been the recipients of the 2013 Student Leadership Award: Forming Partnerships from Brown"
+        
+        sectionBackgroundColor="#FCE794"
+        sectionTextColor="#000"
+      ></Section>
+
     </div>
+
+    
+      
     
   );
+    
+  
 }
